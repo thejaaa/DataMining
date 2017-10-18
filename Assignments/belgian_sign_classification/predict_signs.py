1abf9d11b1e9492e6cadf2f5a9a629a869d29292
thejaswini <theju.94@gmail.com>
2017-10-18 15:46:31 -0500

    Add files via upload

---
+++ Assignments/belgian_sign_classification/predict_signs.py
@@ -0,0 +1,172 @@
+import tensorflow as tf
+import os
+import skimage
+from skimage import data
+import matplotlib.pyplot as plt
+import numpy as np 
+import random
+from skimage import transform
+from skimage.color import rgb2gray
+
+
+
+
+
+def load_data(data_directory):
+    directories = [d for d in os.listdir(data_directory) 
+                   if os.path.isdir(os.path.join(data_directory, d))]
+    labels = []
+    images = []
+    for d in directories:
+        label_directory = os.path.join(data_directory, d)
+        file_names = [os.path.join(label_directory, f) 
+                      for f in os.listdir(label_directory) 
+                      if f.endswith(".ppm")]
+        for f in file_names:
+            images.append(skimage.data.imread(f))
+            labels.append(int(d))
+    return images, labels
+
+
+#my path 
+ROOT_PATH = "/Users/kartikyallapragada/Desktop/Tensor/TT"
+train_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Training")
+test_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Testing")
+
+images, labels = load_data(train_data_directory)
+
+# Make a histogram with 62 bins of the `labels` data
+plt.hist(labels, 62)
+
+# Show the plot
+plt.show()
+unique_labels = set(labels)
+
+# Initialize the figure
+plt.figure(figsize=(15, 15))
+
+# Set a counter
+i = 1
+
+# For each unique label,
+for label in unique_labels:
+    # You pick the first image for each label
+    image = images[labels.index(label)]
+    # Define 64 subplots 
+    plt.subplot(8, 8, i)
+    # Don't include axes
+    plt.axis('off')
+    # Add a title to each subplot 
+    plt.title("Label {0} ({1})".format(label, labels.count(label)))
+    # Add 1 to the counter
+    i += 1
+    # And you plot this first image 
+    plt.imshow(image)
+    
+# Show the plot
+plt.show()
+
+#place holders
+x = tf.placeholder(dtype = tf.float32, shape = [None, 28, 28])
+y = tf.placeholder(dtype = tf.int32, shape = [None])
+
+# Flatten the input data
+images_flat = tf.contrib.layers.flatten(x)
+
+# Fully connected layer 
+logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
+
+# Define a loss function
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, 
+                                                                    logits = logits))
+# Define an optimizer 
+train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+
+# Convert logits to label indexes
+correct_pred = tf.argmax(logits, 1)
+
+# Define an accuracy metric
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+#After importing the transform module
+# Rescale the images in the `images` array
+images28 = [transform.resize(image, (28, 28)) for image in images]
+
+#After importing rgb2gray then
+# Convert `images28` to an array
+images28 = np.array(images28)
+
+# Convert `images28` to grayscale
+images28 = rgb2gray(images28)
+
+#running the neural network
+tf.set_random_seed(1234)
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
+
+for i in range(201):
+    
+        print('EPOCH', i)
+        
+        _, accuracy_value = sess.run([train_op, accuracy], feed_dict={x: images28, y: labels})
+        _, loss_value = sess.run([train_op, loss], feed_dict={x: images28, y: labels})
+        
+        if i % 10 == 0:
+            
+            print("Loss: ", loss_value)
+            print("Accuracy:", accuracy_value)
+       # print('DONE WITH EPOCH')
+        
+        
+#Evaluating the neural network
+sample_indexes = random.sample(range(len(images28)), 10)
+sample_images = [images28[i] for i in sample_indexes]
+sample_labels = [labels[i] for i in sample_indexes]
+
+# Run the "correct_pred" operation
+predicted = sess.run([correct_pred], feed_dict={x: sample_images})[0]
+                        
+# Print the real and predicted labels
+print(sample_labels)
+print(predicted)
+
+# Display the predictions and the ground truth visually.
+fig = plt.figure(figsize=(10, 10))
+for i in range(len(sample_images)):
+    truth = sample_labels[i]
+    prediction = predicted[i]
+    plt.subplot(5, 2,1+i)
+    plt.axis('off')
+    color='green' if truth == prediction else 'red'
+    plt.text(40, 10, "Truth:        {0}\nPrediction: {1}".format(truth, prediction), 
+             fontsize=12, color=color)
+    plt.imshow(sample_images[i],  cmap="gray")
+
+plt.show()
+
+
+# Load the test data
+test_images, test_labels = load_data(test_data_directory)
+
+# Transform the images to 28 by 28 pixels
+test_images28 = [transform.resize(image, (28, 28)) for image in test_images]
+
+
+test_images28 = rgb2gray(np.array(test_images28))
+
+# Run predictions against the full test set.
+predicted = sess.run([correct_pred], feed_dict={x: test_images28})[0]
+
+# Calculate correct matches 
+match_count = sum([int(y == y_) for y, y_ in zip(test_labels, predicted)])
+
+# Calculate the accuracy
+accuracy = match_count / len(test_labels)
+
+# Print the accuracy
+print("Accuracy: {:.3f}".format(accuracy))
+        
+
+
+
